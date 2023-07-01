112eed9fea8bb63a71b2f3f74c3f1e831a2808d0
Atalay Yılmazşimşek <Atalayysimsek9@hotmail.com>
2023-07-02 02:11:32 +0300

    Proje dosyası ekle.

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void Ekle(Musteri musteri)
+        {
+            Console.WriteLine("Müşteri başarıyla eklendi: " + musteri.Ad + " " + musteri.Soyad);
+        }
+
+        public void Listeleme(Musteri musteri)
+        {
+            Console.WriteLine("Müşteri listelendi: " + musteri.Ad + " " + musteri.Soyad);
+        }
+
+        public void Silme(Musteri musteri)
+        {
+            Console.WriteLine("Müşteri silme işlemi başarılı: " + musteri.Ad + " " + musteri.Soyad);
+        }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri1 = new Musteri();
+            musteri1.Ad = "Atalay";
+            musteri1.Soyad = "Yılmazşimşek";
+            musteri1.KayitTarihi = 2023;
+
+            Musteri musteri2 = new Musteri();
+            musteri2.Ad = "Alper";
+            musteri2.Soyad = "Polat";
+            musteri2.KayitTarihi = 2014;
+
+            MusteriManager musteriManager = new MusteriManager();
+            musteriManager.Ekle(musteri1);
+            musteriManager.Ekle(musteri2);
+
+            musteriManager.Listeleme(musteri1);
+            musteriManager.Listeleme(musteri2);
+
+            musteriManager.Silme(musteri1);
+            musteriManager.Silme(musteri2);
+
+
+        }
+    }
+}
